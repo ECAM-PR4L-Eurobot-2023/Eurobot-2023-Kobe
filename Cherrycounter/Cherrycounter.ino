d411cd1bb42b131de6ce1cbd44a8df591f4641b7
olingo99 <engels.theo@skynet.be>
2023-04-13 16:01:02 +0200

    d

--- Cherrycounter/Cherrycounter.ino
+++ Cherrycounter/Cherrycounter.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-// #include <U8g2lib.h>
+#include <U8g2lib.h>
 #include <Wire.h>
 
 #include <WiFi.h>
@@ -37,7 +37,7 @@ ros::NodeHandle nh;
 
 ros::Subscriber<std_msgs::Empty> sub("kobe/getCherry", &getCallback);
 
-// U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE); // High speed I2C
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE); // High speed I2C
 
 
 
@@ -45,16 +45,19 @@ ros::Subscriber<std_msgs::Empty> sub("kobe/getCherry", &getCallback);
 void setup(void)
 {
   Serial.begin(115200);
-  // u8g2.begin();
+  u8g2.begin();
   pinMode(countPin, INPUT);
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
+  unsigned long start = millis();
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED && millis()-start<10000)
   {
     delay(500);
     Serial.println("...");
   }
 
+ if (WiFi.status() == WL_CONNECTED){
   Serial.print("WiFi connected with IP: ");
   Serial.println(WiFi.localIP());
 
@@ -63,33 +66,42 @@ void setup(void)
 
   nh.advertise(sendCherry);
   nh.subscribe(sub);
+ }
+
 }
 
 void loop(void)
 {
+  if (WiFi.status() == WL_CONNECTED){
   nh.spinOnce();
   delay(10);
+  }
 
-  // if (!digitalRead(countPin))
-  // {
-  //   count += 1;
-  //   while (!digitalRead(countPin))
-  //     ;
-  // }
+  // Serial.println(count);
 
-  // if (prevCount != count)
-  // {
-  //   drawn = false;
-  //   prevCount = count;
-  // }
+  if (!digitalRead(countPin))
+  {
+    count += 1;
+    while (!digitalRead(countPin))
+      ;
+  }
 
-  // if (!drawn)
-  // {
-  //   u8g2.clearBuffer();
-  //   u8g2.setFont(u8g2_font_inr24_mn);
-  //   char buf[4];
-  //   sprintf(buf, "%d", count);
-  //   u8g2.drawStr(70 - (20 * ((count > 9 ? 1 : 0) + (count > 99 ? 1 : 0))), 44, buf);
-  //   u8g2.sendBuffer();
-  // }
+  if (prevCount != count)
+  {
+    drawn = false;
+    prevCount = count;
+  }
+
+  if (!drawn)
+  {
+    Serial.println("drawing");
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_inr24_mn);
+    char buf[4];
+    sprintf(buf, "%d", count);
+    u8g2.drawStr(70 - (20 * ((count > 9 ? 1 : 0) + (count > 99 ? 1 : 0))), 44, buf);
+    u8g2.sendBuffer();
+    drawn = true;
+  }
+
 }
