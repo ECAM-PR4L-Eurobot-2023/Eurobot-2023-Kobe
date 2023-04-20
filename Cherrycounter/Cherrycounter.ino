13a3f5511cd72d67a9b4f8ef85ce6bc41e1e6241
olingo99 <engels.theo@skynet.be>
2023-04-20 22:16:32 +0200

    s

--- Cherrycounter/Cherrycounter.ino
+++ Cherrycounter/Cherrycounter.ino
@@ -2,41 +2,19 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 
-#include <WiFi.h>
-
-#define ROSSERIAL_ARDUINO_TCP
-
-#include <ros.h>
-#include <std_msgs/Int16.h>
-#include <std_msgs/Empty.h>
 
 #define countPin 4
 #define REBOUND_TIME 50
 
-const char *ssid = "Eurobot-WiFi";
-const char *password = "robotseverywhere";
 
 uint32_t timeBetweenCounter = 0;
 int count = 0;
 int prevCount = 0;
 boolean drawn = false;
 
-std_msgs::Int16 msgInt;
-
-ros::Publisher sendCherry("kobe/sendCherry", &msgInt);
-void getCallback(const std_msgs::Empty &msg)
-{
-  msgInt.data = count;
-  sendCherry.publish(&msgInt);
-}
-
-IPAddress server(10, 42, 0, 100);
-const uint16_t serverPort = 11411;
-
-ros::NodeHandle nh;
+int score = 25;
 
 
-ros::Subscriber<std_msgs::Empty> sub("kobe/getCherry", &getCallback);
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE); // High speed I2C
 
@@ -45,8 +23,9 @@ void countCherry()
   if ((millis() - timeBetweenCounter) > REBOUND_TIME) {
     timeBetweenCounter = millis();
     count += 1;
-    // Serial.println("yes");
-    // Serial.println(count);
+    if (count>=20){
+      count = 20;
+    }
   }
 }
 
@@ -58,55 +37,34 @@ void setup(void)
   pinMode(countPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(countPin), countCherry, FALLING);
 
-
-  WiFi.begin(ssid, password);
-  unsigned long start = millis();
-  Serial.println("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED && millis()-start<10000)
-  {
-    delay(500);
-    Serial.println("...");
-  }
-
- if (WiFi.status() == WL_CONNECTED){
-  Serial.print("WiFi connected with IP: ");
-  Serial.println(WiFi.localIP());
-
-  nh.getHardware()->setConnection(server, serverPort);
-  nh.initNode();
-
-  nh.advertise(sendCherry);
-  nh.subscribe(sub);
- }
-
 }
 
 void loop(void)
 {
-  if (WiFi.status() == WL_CONNECTED){
-  nh.spinOnce();
-  delay(10);
-  }
-
-  // Serial.println(count);
-
-  // if(!digitalRead(countPin)){
-  //   count += 1;
-  //   while(!digitalRead(countPin));
-  // }
-
   if(prevCount != count){
     drawn = false;
     prevCount = count;
   }
 
-  if(!drawn){
-    // Serial.println("deur");
+
+  if(!drawn ){
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_inr24_mn);
-    char buf[4];
-    sprintf(buf, "%d", count);
-    u8g2.drawStr(70-(20* ((count>9? 1: 0)+(count>99? 1: 0))),44,buf);
+    char bufCherries[13];
+    char bufScore[11];
+    sprintf(bufCherries, "Cherries :%d", count);
+    if (count!=0){
+      sprintf(bufScore, "Score :%d", score+5+count);
+    }
+    else{
+      sprintf(bufScore, "Score :%d", score);
+    }
+    
+    // u8g2.drawStr(70-(20* ((count>9? 1: 0)+(count>99? 1: 0))),10,buf);
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(0, 20,bufCherries);
+    u8g2.drawStr(0, 50, bufScore);
+
     u8g2.sendBuffer();
   }
 
